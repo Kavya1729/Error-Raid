b1664b57784f190a0a59c8c5291d4ae2cc2a8a8c
Kavya Mittal <154065568+Kavya1729@users.noreply.github.com>
2025-03-04 22:19:56 +0530

    Update ai.routes.js

--- Backend/src/routes/ai.routes.js
+++ Backend/src/routes/ai.routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const aiController = require('../controllers/ai.controller');
 const auth = require('../middleware/auth');
 
-router.post('/get-solution',auth,aiController.getResponse) 
+router.post('/get-solution',aiController.getResponse) 
 
 module.exports = router;
